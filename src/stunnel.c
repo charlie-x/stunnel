382e9b6dbb27d24bcefec11478ef1ca9eb324eab
charliex <charlie@finitemonkeys.com>
2022-08-03 13:32:08 -0700

    removed applink.c?

--- src/stunnel.c
+++ src/stunnel.c
@@ -47,7 +47,7 @@
 #pragma GCC diagnostic ignored "-Wpedantic"
 #endif /* __GNUC__ */
 
-#include "openssl/applink.c"
+//#include "openssl/applink.c"
 
 #ifdef __GNUC__
 #if __GNUC__ > 4 || (__GNUC__ == 4 && __GNUC_MINOR__ >= 6)
